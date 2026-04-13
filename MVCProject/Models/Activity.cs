b5f11a79d2660e8d4cbed8165a3700c7e6d2221b
Omar Hany <ohany3051@gmail.com>
2026-04-13 19:06:31 +0200

    Add Flight, Accomodation and Activity Models

---
+++ MVCProject/Models/Activity.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCProject.Models
+{
+    public class Activity
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(2)]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        [MinLength(2)]
+        [MaxLength(100)]
+        public string Location { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        [Required]
+        [Range(0, 1000)]
+        public decimal Price { get; set; }
+
+        [Required]
+        [Range(0, 500)]
+        public int Capacity { get; set; }
+
+    }
+}
