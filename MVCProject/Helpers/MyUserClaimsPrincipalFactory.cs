d84b6db2b7fc111b05012d2e1374145e8ff664f3
ITI-Student <shady.mohamed7899@gmail.com>
2026-04-26 23:17:25 +0300

    Update Claims

--- MVCProject/Helpers/MyUserClaimsPrincipalFactory.cs
+++ MVCProject/Helpers/MyUserClaimsPrincipalFactory.cs
@@ -1,12 +1,14 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿ using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Options;
 using MVCProject.Models;
 using System.Security.Claims;
 
 namespace MVCProject.Helpers {
-    public class MyUserClaimsPrincipalFactory : UserClaimsPrincipalFactory<AppUser> {
-        public MyUserClaimsPrincipalFactory(UserManager<AppUser> userManager, IOptions<IdentityOptions> optionsAccessor) : 
-            base(userManager, optionsAccessor) {
+    public class MyUserClaimsPrincipalFactory : UserClaimsPrincipalFactory<AppUser, IdentityRole> {
+        public MyUserClaimsPrincipalFactory(UserManager<AppUser> userManager,
+            RoleManager<IdentityRole> roleManager,
+            IOptions<IdentityOptions> options) : 
+            base(userManager, roleManager, options) {
         }
 
         protected override async Task<ClaimsIdentity> GenerateClaimsAsync(AppUser user) {
