3fecdc9b5cf6a78be6bb14766ef4c52d2a98fa6a
Omar-Nabil2 <omarabosalem66@gmail.com>
2026-04-29 00:34:55 +0300

    add isBanned

--- MVCProject/Models/AppUser.cs
+++ MVCProject/Models/AppUser.cs
@@ -6,6 +6,7 @@ namespace MVCProject.Models {
         public string FullName { get; set; }
         public string Address { get; set; }
         public string PhoneNumber { get; set; }
+        public bool IsBanned { get; set; } = false;
 
         public ICollection<Booking>? Bookings { get; set; } = new HashSet<Booking>();
     }
